93881dfebd115d1970aaa40b05ccfd57aabd342e
Ary Othaz <aryothaz@gmail.com>
2021-02-25 00:35:47 -0300

    ble server v3

--- src/App.js
+++ src/App.js
@@ -2,109 +2,103 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [supportsBluetooth, setSupportsBluetooth] = useState(false);
-  const [isDisconnected, setIsDisconnected] = useState(true);
-  const [batteryLevel, setBatteryLevel] = useState(null);
+    const BLEService_UUID  = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+    const BLECharRX_UUID   = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+    const BLECharTX_UUID   = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
-  // When the component mounts, check that the browser supports Bluetooth
-  useEffect(() => {
-    if (navigator.bluetooth) {
-      setSupportsBluetooth(true);
-    }
-  }, []);
+    const [device, setDevice] = useState(null);
+    const [server, setServer] = useState(null);
+    const [service, setService] = useState(null);
+    const [rxCharacteristic, setRxCharacteristic] = useState(null);
+    const [txCharacteristic, setTxCharacteristic] = useState(null);
+    const [connected, setConnected] = useState(false);
 
-  /**
-   * Let the user know when their device has been disconnected.
-   */
-  const onDisconnected = (event) => {
-    alert(`The device ${event.target} is disconnected`);
-    setIsDisconnected(true);
-  }
+    const [supportsBluetooth, setSupportsBluetooth] = useState(false);
 
-  /**
-   * Update the value shown on the web page when a notification is
-   * received.
-   */
-  const handleCharacteristicValueChanged = (event) => {
-    setBatteryLevel(event.target.value.getUint8(0) + '%');
-  }
-
-  /**
-   * Attempts to connect to a Bluetooth device and subscribe to
-   * battery level readings using the battery service.
-   */
-  const connectToDeviceAndSubscribeToUpdates = async () => {
-    navigator.bluetooth.requestDevice({
-        optionalServices: [bleNusServiceUUID],
-        acceptAllDevices: true
-    })
-    .then(device => {
-        bleDevice = device; 
-        console.log('Found ' + device.name);
-        console.log('Connecting to GATT Server...');
-        bleDevice.addEventListener('gattserverdisconnected', onDisconnected);
-        return device.gatt.connect();
-    })
-    .then(server => {
-        console.log('Locate NUS service');
-        return server.getPrimaryService(bleNusServiceUUID);
-    }).then(service => {
-        nusService = service;
-        console.log('Found NUS service: ' + service.uuid);
-    })
-    .then(() => {
-        console.log('Locate RX characteristic');
-        return nusService.getCharacteristic(bleNusCharRXUUID);
-    })
-    .then(characteristic => {
-        rxCharacteristic = characteristic;
-        console.log('Found RX characteristic');
-    })
-    .then(() => {
-        console.log('Locate TX characteristic');
-        return nusService.getCharacteristic(bleNusCharTXUUID);
-    })
-    .then(characteristic => {
-        txCharacteristic = characteristic;
-        console.log('Found TX characteristic');
-    })
-    .then(() => {
-        console.log('Enable notifications');
-        return txCharacteristic.startNotifications();
-    })
-    .then(() => {
-        console.log('Notifications started');
-        txCharacteristic.addEventListener('characteristicvaluechanged',
-                                          handleNotifications);
-        connected = true;
-        window.term_.io.println('\r\n' + bleDevice.name + ' Connected.');
-        nusSendString('\r');
-        setConnButtonState(true);
-    })
-    .catch(error => {
-        console.log('' + error);
-        window.term_.io.println('' + error);
-        if(bleDevice && bleDevice.gatt.connected)
-        {
-            bleDevice.gatt.disconnect();
+    useEffect(() => {
+        if (navigator.bluetooth) {
+        setSupportsBluetooth(true);
         }
-    });
-  };
+    }, []);
 
-  return (
-    <div className="App">
-      <h1>Get Device Battery Info Over Bluetooth</h1>
-      {supportsBluetooth && !isDisconnected &&
-            <p>Battery level: {batteryLevel}</p>
-      }
-      {supportsBluetooth && isDisconnected &&
-        <button onClick={connectToDeviceAndSubscribeToUpdates}>Connect to a Bluetooth device</button>
-      }
-      {!supportsBluetooth &&
-        <p>This browser doesn't support the Web Bluetooth API</p>
-      }
-    </div>
-  );
+    const connectToDevice = async () => {
+        navigator.bluetooth.requestDevice({
+            optionalServices: [BLEService_UUID],
+            acceptAllDevices: true
+        })
+        .then(device => {
+            setDevice(device); 
+            console.log('Found ' + device.name);
+            console.log('Connecting to GATT Server...');
+            device.addEventListener('gattserverdisconnected', onDisconnected);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log('Locate NUS service');
+            return server.getPrimaryService(BLEService_UUID);
+        })
+        .then(service => {
+            setService(service); 
+            console.log('Found NUS service: ' + service.uuid);
+        })
+        .then(() => {
+            console.log('Locate RX characteristic');
+            return service.getCharacteristic(BLECharRX_UUID);
+        })
+        .then(characteristic => {
+            rxCharacteristic = characteristic;
+            console.log('Found RX characteristic');
+        })
+        .then(() => {
+            console.log('Locate TX characteristic');
+            return service.getCharacteristic(BLECharTX_UUID);
+        })
+        .then(characteristic => {
+            txCharacteristic = characteristic;
+            console.log('Found TX characteristic');
+        })
+        .then(() => {
+            console.log('Enable notifications');
+            return txCharacteristic.startNotifications();
+        })
+        .then(() => {
+            console.log('Notifications started');
+            txCharacteristic.addEventListener('characteristicvaluechanged',
+                                            handleNotifications);
+            setConnected(true);
+            window.term_.io.println('\r\n' + device.name + ' Connected.');
+        })
+        .catch(error => {
+            console.log('' + error);
+            if(device && device.gatt.connected)
+            {
+                device.gatt.disconnect();
+            }
+        });
+    };
+
+    function onDisconnected() {
+        setConnected(false);
+        window.term_.io.println('\r\n' + device.name + ' Disconnected.');
+    }
+
+    function handleNotifications(event) {
+        console.log('notification');
+        let value = event.target.value;
+        let str = "";
+        for (let i = 0; i < value.byteLength; i++) {
+            str += String.fromCharCode(value.getUint8(i));
+        }
+    }
+
+    return (
+        <div className="App">
+        <h1>Get Device Battery Info Over Bluetooth</h1>
+        {supportsBluetooth &&
+            <button onClick={connectToDevice}>Connect to a Bluetooth device</button>
+        }
+        </div>
+    );
 }
 
 export default App;
